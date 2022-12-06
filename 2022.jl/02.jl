249389358c5a52843c3bfecfd01bfa915b02f594
Lukáš Malý <lukasmaly@gmail.com>
2022-12-06 09:40:45 +0100

    Update Day 2 of 2022

--- 2022.jl/02.jl
+++ 2022.jl/02.jl
@@ -3,77 +3,38 @@ Day 2: Rock Paper Scissors
 https://adventofcode.com/2022/day/2
 """
 
-function part1_outcome(left, right)
-    if right == "X"
-        if left == "A"
-            outcome = 3
-        elseif left == "B"
-            outcome = 0
-        else  # left == "C"
-            outcome = 6
-        end
-        return outcome + 1
-    elseif right == "Y"
-        if left == "A"
-            outcome = 6
-        elseif left == "B"
-            outcome = 3
-        else  # left == "C"
-            outcome = 0
-        end
-        return outcome + 2
-    else  # right == "Z"
-        if left == "A"
-            outcome = 0
-        elseif left == "B"
-            outcome = 6
-        else  # left == "C"
-            outcome = 3
-        end
-        return outcome + 3
-    end
-end
-
-function part2_outcome(left, right)
-    if right == "X"
-        if left == "A"
-            outcome = 3
-        elseif left == "B"
-            outcome = 1
-        else  # left == "C"
-            outcome = 2
-        end
-        return outcome + 0
-    elseif right == "Y"
-        if left == "A"
-            outcome = 1
-        elseif left == "B"
-            outcome = 2
-        else  # left == "C"
-            outcome = 3
-        end
-        return outcome + 3
-    else  # right == "Z"
-        if left == "A"
-            outcome = 2
-        elseif left == "B"
-            outcome = 3
-        else  # left == "C"
-            outcome = 1
-        end
-        return outcome + 6
-    end
-end
-
 function part1(lines)
+    outcome = Dict{String, Dict{String, Int8}}(
+        "X" => Dict("A" => 3 + 1,
+                    "B" => 0 + 1,
+                    "C" => 6 + 1),
+        "Y" => Dict("A" => 6 + 2,
+                    "B" => 3 + 2,
+                    "C" => 0 + 2),
+        "Z" => Dict("A" => 0 + 3,
+                    "B" => 6 + 3,
+                    "C" => 3 + 3)
+    )
     rounds = map(line -> split(line, ' '), lines)
-    outcomes = map(x -> part1_outcome(x[1], x[2]), rounds)
+    outcomes = map(x -> outcome[x[2]][x[1]], rounds)
     return sum(outcomes)
 end
 
 function part2(lines)
+    outcome = Dict{String, Dict{String, Int8}}(
+        "X" => Dict("A" => 3 + 0,
+                    "B" => 1 + 0,
+                    "C" => 2 + 0),
+        "Y" => Dict("A" => 1 + 3,
+                    "B" => 2 + 3,
+                    "C" => 3 + 3),
+        "Z" => Dict("A" => 2 + 6,
+                    "B" => 3 + 6,
+                    "C" => 1 + 6)
+    )
     rounds = map(line -> split(line, ' '), lines)
-    outcomes = map(x -> part2_outcome(x[1], x[2]), rounds)
+    @show rounds
+    outcomes = map(x -> outcome[x[2]][x[1]], rounds)
     return sum(outcomes)
 end
 
