3f44ba59b967749d35a7530d860235f1f18d2f7a
Lukáš Malý <lukasmaly@gmail.com>
2022-12-09 20:08:12 +0100

    Update Day 8 of 2022

--- 2022.jl/08.jl
+++ 2022.jl/08.jl
@@ -11,17 +11,35 @@ function part1(lines)
     height_map = read_map(lines)
     m, n = size(height_map)
     visibility_map = zeros(Bool, (m, n))
-    for k in [0, 1, 1, 1]
-        height_map = rotl90(height_map, k)
-        visibility_map = rotl90(visibility_map, k)
-        for j = 1:n
-            max_height = height_map[1, j]
-            visibility_map[1, j] = true
-            for i = 2:m
-                if height_map[i, j] > max_height
-                    visibility_map[i, j] = true
-                    max_height = height_map[i, j]
-                end
+    for j = 1:n
+        max_height1 = height_map[1, j]
+        visibility_map[1, j] = true
+        max_height2 = height_map[end, j]
+        visibility_map[end, j] = true
+        for (i1, i2) in zip(2:m-1, m-1:-1:2)
+            if height_map[i1, j] > max_height1
+                visibility_map[i1, j] = true
+                max_height1 = height_map[i1, j]
+            end
+            if height_map[i2, j] > max_height2
+                visibility_map[i2, j] = true
+                max_height2 = height_map[i2, j]
+            end
+        end
+    end
+    for i = 1:m
+        max_height1 = height_map[i, 1]
+        visibility_map[i, 1] = true
+        max_height2 = height_map[i, end]
+        visibility_map[i, end] = true
+        for (j1, j2) in zip(2:n-1, n-1:-1:2)
+            if height_map[i, j1] > max_height1
+                visibility_map[i, j1] = true
+                max_height1 = height_map[i, j1]
+            end
+            if height_map[i, j2] > max_height2
+                visibility_map[i, j2] = true
+                max_height2 = height_map[i, j2]
             end
         end
     end
